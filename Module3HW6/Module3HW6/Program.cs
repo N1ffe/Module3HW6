9a05f80f3652f4f22edfb2109e6f0e57e132077c
Niffe <petrovnik01@ukr.net>
2022-12-10 16:10:00 +0200

    Add empty project

---
+++ Module3HW6/Module3HW6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module3HW6
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
